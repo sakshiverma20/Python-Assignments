e2b62c8171a6ed164a25b3101d34855ab8be23f7
sakshiverma20 <vsakshi629@gmail.com>
2025-06-29 22:12:38 +0530

    Added all the assignments with their tasks to the respective folder

---
+++ Assignment3/assignment3_task2.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+import math
+sqrt = math.sqrt(num)
+log = math.log(num)
+sin = math.sin(num)
+print("Square root: ",sqrt)
+print("Logarithm: ",log)
+print("Sine: ",sin)
+
+
