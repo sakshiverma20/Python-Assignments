e4fe3a5967222bcd984cc7ab0a9677d93eac77b7
sakshiverma20 <vsakshi629@gmail.com>
2025-07-03 00:19:02 +0530

    removed assignment5

--- Assignment5/assignment5_task1.py
+++
@@ -1,6 +0,0 @@
-dict = {"Alice": 85}
-stud_name = input("Enter the student's name: ")
-if stud_name in dict:
-    print(f"{stud_name}'s marks: {dict[stud_name]}")
-else:
-    print("Student not found")
